5aef3150cbd3181f0616e8bed3f970e713f611d0
Iris System <iris@iris.ac.nz>
2021-12-04 01:00:37 +1300

    misc: Add PHP CLI server hack to public/index.php

--- public/index.php
+++ public/index.php
@@ -2,6 +2,12 @@
 declare(strict_types=1);
 require_once(dirname(dirname(($p = realpath(__FILE__)) === false ? __FILE__ : $p)) . '/bootstrap.php');
 
+/* Hack to allow PHP development server to serve static files */
+if (php_sapi_name() === 'cli-server') {
+	$fileName = dirname(__FILE__) . parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+	if (file_exists($fileName) && !is_dir($fileName)) return false;
+}
+
 /* Load configuration */
 \PKAwake\Configuration::load();
 
